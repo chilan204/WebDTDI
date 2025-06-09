89afd34b0892bd27f06228735956035f12f4ff44
trungnb <cstrung6@gmail.com>
2025-06-09 22:54:28 +0700

    add infor db in oracal

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO C##DTDI.member (id, username, email, em_code, password, role, created_date, modified_date)
+INSERT INTO WEB_DTDI.MEMBER (id, username, email, em_code, password, role, created_date, modified_date)
 VALUES (1, 'admin', 'admin@example.com', 'EM001', '$10$OStFgbodvUnXIWr1rLfGqeEGgmtuvzPq6CPTPViO0/w1VIyysCVba', 'ADMIN', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
